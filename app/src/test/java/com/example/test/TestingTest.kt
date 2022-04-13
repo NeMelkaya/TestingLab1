74fcd42c447f939e421d3f18b7f961cdba5f79ae
NeMelkaya <cumka-super@mail.ru>
2022-04-13 21:10:08 +0700

    rename tests

--- app/src/test/java/com/example/test/TestingTest.kt
+++ app/src/test/java/com/example/test/TestingTest.kt
@@ -7,49 +7,49 @@ import org.junit.Test
 class TestingTest{
 
     @Test
-    fun test1(){
-        val n = 23
+    fun `positive test with maximum allowable n`(){
+        val n = 29
         val S = intArrayOf(2, 3, 5)
-        assertEquals("-1--2--3-", Testing.fromNb2Str(n, S))
+        assertEquals("-1--2--4-", Testing.fromNb2Str(n, S))
     }
 
     @Test
-    fun test2(){
+    fun `positive test with minimum allowable n`(){
         val n = 0
         val S = intArrayOf(23, 51)
         assertEquals("-0--0-", Testing.fromNb2Str(n, S))
     }
 
     @Test
-    fun test3(){
+    fun `modules are not pairwise coprime`(){
         val n = 33
         val S = intArrayOf(2, 3, 5, 25)
         assertEquals("Not applicable", Testing.fromNb2Str(n, S))
     }
 
     @Test
-    fun test4(){
+    fun `the product of modules is less than the number n`(){
         val n = 555
         val S = intArrayOf(2, 5, 11)
         assertEquals("Not applicable", Testing.fromNb2Str(n, S))
     }
 
     @Test
-    fun test5(){
+    fun `the product of modules is equal to the number n`(){
         val n = 110
         val S = intArrayOf(2, 5, 11)
         assertEquals("Not applicable", Testing.fromNb2Str(n, S))
     }
 
     @Test
-    fun test6(){
+    fun `the number n is negative`(){
         val n = -343
         val S = intArrayOf(2, 3, 5)
         assertEquals("Not applicable", Testing.fromNb2Str(n, S))
     }
 
     @Test
-    fun test7(){
+    fun `the array of modules is empty`(){
         val n = 25
         val S = intArrayOf()
         assertEquals("Not applicable", Testing.fromNb2Str(n, S))
